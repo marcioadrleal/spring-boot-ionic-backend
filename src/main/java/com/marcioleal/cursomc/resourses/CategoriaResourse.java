fe072379cd9205a5f0c0af952e7c03a7cd7da4c4
Marcio Leal <marcioadrleal@gmail.com>
2018-03-27 16:06:21 -0300

    Rest testado

---
+++ src/main/java/com/marcioleal/cursomc/resourses/CategoriaResourse.java
@@ -0,0 +1,16 @@
+package com.marcioleal.cursomc.resourses;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResourse {
+
+  @RequestMapping(method=RequestMethod.GET)	
+  public String listar() {
+	return "Rest está funcionando";  
+  }	
+	
+}
