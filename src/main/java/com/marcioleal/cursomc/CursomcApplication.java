17cc1f6df5fd8b1835cc77e577f2e0f6c414f60a
Marcio Leal <marcioadrleal@gmail.com>
2018-03-28 16:22:15 -0300

    Inserir dados no banco

--- src/main/java/com/marcioleal/cursomc/CursomcApplication.java
+++ src/main/java/com/marcioleal/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.marcioleal.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.marcioleal.cursomc.domain.Categoria;
+import com.marcioleal.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepositiry;
+	
 	public static void main(String[] args) {
 		
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	  Categoria cat1 = new Categoria(null,"Informática");
+	  Categoria cat2 = new Categoria(null,"Escritorio");
+	  categoriaRepositiry.save(Arrays.asList(cat1,cat2));
+	}
+	
+	
 }
