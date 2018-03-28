4e9c7c996de2fe29241dbd28732ef05b807c992c
Marcio Leal <marcioadrleal@gmail.com>
2018-03-28 13:12:49 -0300

    Banco H2 e Criação automatica da basa de dados

--- src/main/java/com/marcioleal/cursomc/domain/Categoria.java
+++ src/main/java/com/marcioleal/cursomc/domain/Categoria.java
@@ -2,12 +2,22 @@ package com.marcioleal.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Categoria implements Serializable {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 7669129827898644291L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
