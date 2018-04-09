3056366c9dc71141e93d3b1dda94a70ec002f058
Marcio Leal <marcioadrleal@gmail.com>
2018-04-09 15:58:53 -0300

    Configuração inicial Spring Security

--- src/main/java/com/marcioleal/cursomc/config/DevConfig.java
+++ src/main/java/com/marcioleal/cursomc/config/DevConfig.java
@@ -24,7 +24,7 @@ public class DevConfig {
 	public boolean instantiateDatabase() throws ParseException {
 		if ( !"create".equals(strategy)) {
 		  return false;	
-		}
+	    }
 		
 		dbService.instantiateDatabase();
 		return true;
