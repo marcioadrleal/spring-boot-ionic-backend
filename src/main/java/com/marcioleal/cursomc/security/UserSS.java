a85c307935b9d6b441f8398bf8bc652a75906381
Marcio Leal <marcioadrleal@gmail.com>
2018-04-10 14:46:16 -0300

    Autorizações Atualizadas

--- src/main/java/com/marcioleal/cursomc/security/UserSS.java
+++ src/main/java/com/marcioleal/cursomc/security/UserSS.java
@@ -11,10 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import com.marcioleal.cursomc.domain.enums.Perfil;
 
 public class UserSS implements UserDetails {
-
-	/**
-	 * 
-	 */
+	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
 	private String email;
@@ -22,12 +19,9 @@ public class UserSS implements UserDetails {
 	private Collection<? extends GrantedAuthority> authorities;
 	
 	public UserSS() {
-		
 	}
 	
-	
-	
-	public UserSS(Integer id, String email, String senha, Set<Perfil> perfis ) {
+	public UserSS(Integer id, String email, String senha, Set<Perfil> perfis) {
 		super();
 		this.id = id;
 		this.email = email;
@@ -35,78 +29,46 @@ public class UserSS implements UserDetails {
 		this.authorities = perfis.stream().map(x -> new SimpleGrantedAuthority(x.getDescricao())).collect(Collectors.toList());
 	}
 
-	private static final long serialVersionUID = -7693111132772543329L;
-
-	
-	
 	public Integer getId() {
 		return id;
 	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
-		this.authorities = authorities;
-	}
-
+	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		return authorities;
 	}
 
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
 		return senha;
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
 		return email;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
 		return true;
 	}
-
+	
+	public boolean hasRole(Perfil perfil) {
+		return getAuthorities().contains(new SimpleGrantedAuthority(perfil.getDescricao()));
+	}
 }
